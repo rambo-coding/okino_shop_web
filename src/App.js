e81ca7143e80a03a7f1a49ea22346588a99aa76b
Na-Coding <krittaphat_na@hotmail.com>
2022-05-26 00:27:48 +0700

    remove nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
-      <NavBar/>
       <HomePage/>
       <Footer/>
     </div>
