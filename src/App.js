98be95e12aaef15fa7b254cb8aed6181e1a84cb9
Na-Coding <krittaphat_na@hotmail.com>
2022-05-26 00:31:40 +0700

    new nevbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar"
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <HomePage/>
       <Footer/>
     </div>
