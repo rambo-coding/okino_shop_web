bd006d3081e8fe5bac47cd23eef97c9154fdab7b
Na-Coding <krittaphat_na@hotmail.com>
2022-05-25 23:43:44 +0700

    change web

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <HomePage />
+      <HomePage/>
       <Footer/>
     </div>
   );
