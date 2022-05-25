244062b7b1bb0b5d72ab67a564c7d3ec4c92fbfe
Na-Coding <krittaphat_na@hotmail.com>
2022-05-25 22:10:20 +0700

    upd components

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="blog-footer">
+        <p>
+          Copyright 2022 © Okino Shop
+        </p>
+        {/* <p>
+          <a href="#">Back to top</a>
+        </p> */}
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
